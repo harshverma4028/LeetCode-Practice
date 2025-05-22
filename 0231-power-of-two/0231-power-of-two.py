271c57a03b8f4c7594bda11a457c3dc9165826f9
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-05-22 11:38:43 +0530

    Time: 0 ms (100%), Space: 12.4 MB (51.1%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -4,10 +4,22 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        i = 0
-        while (2**i <= n):
-            if n == (2**i):
-                return True
-            i +=1
-        return False        
+        # i = 0
+        # while (2**i <= n):
+        #     if n == (2**i):
+        #         return True
+        #     i +=1
+        # return False        
           
+        if n < 1:
+            return False
+        elif n ==1 :
+            return True
+        else:
+            while n%2 ==0:
+                n = n // 2
+            if n == 1:
+                return True
+            else:
+                return False              
+
