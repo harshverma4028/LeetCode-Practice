e27250d6fbcdabb65bf93e3e52dc632bf41b11f2
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-06-24 10:24:16 +0530

    Time: 4 ms (47.21%), Space: 12.3 MB (99.46%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        from collections import Counter
+        count = Counter(s)
+        length = 0
+        odd_found = False
+
+        for char_count in count.values():
+            if char_count % 2 == 0:
+                length += char_count
+            else:
+                length += char_count - 1
+                odd_found = True
+
+        if odd_found:
+            return length + 1
+        else:
+            return length                 
