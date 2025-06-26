2be40bf91b068a8cd952bbe0fe64828fb597a6e4
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-06-26 08:26:20 +0530

    Time: 0 ms (100%), Space: 12.8 MB (92.99%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.py
+++ 0414-third-maximum-number/0414-third-maximum-number.py
@@ -4,10 +4,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        nums = list(set(nums)) 
-        nums.sort(reverse = True)
+        # nums = list(set(nums)) 
+        # nums.sort(reverse = True)
 
-        if len(nums) < 3 :
-            return nums[0]
-        else:
-            return nums[2]    
+        # if len(nums) < 3 :
+        #     return nums[0]
+        # else:
+        #     return nums[2]  
+
+        f = s = t = float('-inf') 
+
+        for num in nums:
+            if num == f or num == s or num == t:
+                continue
+            elif num > f:
+                t = s
+                s = f 
+                f = num
+            elif num > s:
+                t = s
+                s = num
+            elif num > t:
+                t = num
+        return f if t == float('-inf') else t                    
