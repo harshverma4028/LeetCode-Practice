baf08787130b5a5b3b6e7639cc1599ddb08228cb
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-07-28 08:50:41 +0530

    Time: 36 ms (64.83%), Space: 14 MB (48.51%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        res = 0
+        for i in range(0,len(nums)-1,+2):
+            res += nums[i]
+
+        return res    
