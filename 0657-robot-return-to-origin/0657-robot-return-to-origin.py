e3ebd9375050d3a3ef416ea2305e36e45cd75e28
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-08-16 10:45:11 +0530

    Time: 31 ms (56.99%), Space: 12.6 MB (76.64%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -4,15 +4,24 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        l=r=d=u = 0
-        for char in moves:
-            if char == 'L':
-                l += 1
-            elif char == 'U':
-                u += 1
-            elif char == 'R':
-                r += 1
-            elif char == 'D':
-                d += 1
+        # l=r=d=u = 0
+        # for char in moves:
+        #     if char == 'L':
+        #         l += 1
+        #     elif char == 'U':
+        #         u += 1
+        #     elif char == 'R':
+        #         r += 1
+        #     elif char == 'D':
+        #         d += 1
             
-        return u == d and l == r
+        # return u == d and l == r
+
+        x = y = 0
+        for char in moves:
+            if char == 'L': x -= 1
+            elif char == 'U': y += 1
+            elif char == 'R': x += 1
+            elif char == 'D': y -= 1
+
+        return x == 0 and y == 0
