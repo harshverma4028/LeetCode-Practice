4d0b72e396ddf304bdfd0e1dad04e7e6b5b78bf6
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-09-10 09:48:21 +0530

    Time: 46 ms (43.53%), Space: 29.2 MB (24.25%) - LeetHub

---
+++ 0932-monotonic-array/0932-monotonic-array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        is_incr = True
+        is_dcr = True
+
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i-1]:
+                is_incr = False
+            if nums[i] > nums[i-1]:
+                is_dcr = False
+
+        return is_dcr or is_incr
+        
+
+        # def is_incr(nums):
+        #     for i in range(1 ,len(nums)):
+        #         if nums[i-1] > nums[i]:
+        #             return False
+        #     return True
+        
+        # def is_dcr(nums):
+        #     for i in range(1, len(nums)):
+        #         if nums[i-1] < nums[i]:
+        #             return False
+        #     return True
+
+        # return is_incr(nums) or is_dcr(nums)
