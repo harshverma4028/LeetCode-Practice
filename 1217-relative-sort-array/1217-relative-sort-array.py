44eacab22dceead81a814f75ed1f261e72479a23
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-10-03 12:28:41 +0530

    Time: 0 ms (100%), Space: 17.9 MB (39.14%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -1,18 +1,35 @@
+from collections import Counter
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         
-                
         res = []
-        for num in arr2:
-            for x in arr1:
-                if x == num:
-                    res.append(x)
+        freq = Counter(arr1)
+        for x in arr2:
+            res.extend([x] * freq[x])
+            del freq[x]
         
         rem = []
-        for x in arr1:
-            if x not in arr2:
-                rem.append(x)
 
-        rem.sort()
-        res.extend(rem)
+        for num,count in freq.items():
+            rem.extend([num] * count)
+
+        res.extend(sorted(rem))
+
         return res
+
+
+
+        # res = []
+        # for num in arr2:
+        #     for x in arr1:
+        #         if x == num:
+        #             res.append(x)
+        
+        # rem = []
+        # for x in arr1:
+        #     if x not in arr2:
+        #         rem.append(x)
+
+        # rem.sort()
+        # res.extend(rem)
+        # return res
