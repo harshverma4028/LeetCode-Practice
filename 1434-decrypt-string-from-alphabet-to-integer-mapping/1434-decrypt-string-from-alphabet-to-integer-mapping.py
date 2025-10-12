8e1b47cfe71a9f80402b61008e24a4c7c3639522
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-10-12 11:36:01 +0530

    Time: 3 ms (15.86%), Space: 17.9 MB (35.31%) - LeetHub

---
+++ 1434-decrypt-string-from-alphabet-to-integer-mapping/1434-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -0,0 +1,26 @@
+class Solution:
+    def freqAlphabets(self, s: str) -> str:
+        ns =  s.split("#")
+        res = ""
+        char_map = {
+            "1": "a", "2": "b", "3": "c", "4": "d", "5": "e",
+            "6": "f", "7": "g", "8": "h", "9": "i",
+            "10": "j", "11": "k", "12": "l", "13": "m",
+            "14": "n", "15": "o", "16": "p", "17": "q",
+            "18": "r", "19": "s", "20": "t", "21": "u",
+            "22": "v", "23": "w", "24": "x", "25": "y",
+            "26": "z"
+        }
+        i = 0
+        while i < len(s):
+
+            if i + 2 < len(s) and s[i + 2] == '#':
+                num = s[i:i + 2]     
+                res += char_map[num]
+                i += 3               
+            else:
+                num = s[i]
+                res += char_map[num]
+                i += 1
+
+        return res
