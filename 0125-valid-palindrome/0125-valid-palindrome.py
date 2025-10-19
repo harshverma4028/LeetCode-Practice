746e5205f8614765e248eba2ee147d2d1c4f4057
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-10-19 18:05:06 +0530

    Time: 7 ms (82.22%), Space: 17.9 MB (82.65%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,17 +1,17 @@
-import re
-class Solution(object):
-    def isPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        s = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
-        i,j = 0,len(s)-1
-        if len(s) == 1:
-            return True
-        while(i <= j):
-            if s[i] != s[j]:
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        i, j = 0, len(s) - 1
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+            if s[i].lower() != s[j].lower():
                 return False
-            i +=1
-            j -=1
-        return True   
+            i += 1
+            j -= 1
+
+        return True
+
+
