740f82c16b04f5fbca183578325805ee6cd8a572
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-11-16 21:57:55 +0530

    Time: 2 ms (12.47%), Space: 17.9 MB (29.21%) - LeetHub

---
+++ 1400-find-winner-on-a-tic-tac-toe-game/1400-find-winner-on-a-tic-tac-toe-game.py
@@ -0,0 +1,38 @@
+from typing import List , Set
+class Solution:
+    def tictactoe(self, moves: List[List[int]]) -> str:
+        A_moves: Set[tuple] = set()
+        B_moves: Set[tuple] = set()
+        for i in range(len(moves)):
+            my_tuple = tuple(moves[i])
+            if i % 2  == 0:
+                A_moves.add(my_tuple)
+            else:
+                B_moves.add(my_tuple)
+
+
+        winning_patterns = [
+        {(0,0), (0,1), (0,2)},   
+        {(1,0), (1,1), (1,2)},    
+        {(2,0), (2,1), (2,2)},    
+
+        {(0,0), (1,0), (2,0)},    
+        {(0,1), (1,1), (2,1)},   
+        {(0,2), (1,2), (2,2)},    
+
+        {(0,0), (1,1), (2,2)},    
+        {(0,2), (1,1), (2,0)}     
+        ]
+
+        for pattern in winning_patterns:
+            if pattern.issubset(A_moves):
+                return "A"
+            
+            if pattern.issubset(B_moves):
+                return "B"
+        
+        if len(moves) == 9:
+            return "Draw"
+        else:
+            return "Pending"
+
