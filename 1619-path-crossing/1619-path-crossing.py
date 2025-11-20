d6bb0861c5a1c75e4468544fa42f373f8b7d905a
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-11-20 20:41:57 +0530

    Time: 0 ms (100%), Space: 17.7 MB (74.83%) - LeetHub

---
+++ 1619-path-crossing/1619-path-crossing.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        x = y = 0
+        visited = set()
+
+        for chr in path:
+            visited.add((0,0))
+            if chr == "N":
+                y += 1
+            
+            elif chr == "S":
+                y -= 1
+            elif chr == "E":
+                x += 1
+            elif chr == "W":
+                x -= 1
+            
+            if ((x,y)) in visited:
+                return True
+
+            visited.add((x,y))
+        
+        return False
