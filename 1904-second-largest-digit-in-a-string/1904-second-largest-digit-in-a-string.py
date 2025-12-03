9ac1e40707a3b93029f6cee6809f3a0afcbe325c
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-12-03 21:02:54 +0530

    Time: 3 ms (80.93%), Space: 17.6 MB (85.21%) - LeetHub

--- 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
+++ 1904-second-largest-digit-in-a-string/1904-second-largest-digit-in-a-string.py
@@ -3,11 +3,12 @@ class Solution:
         f = -1
         s_ = -1
         for char in s:
-            if char.isnumeric():
-                if int(char) > f:
+            if char.isdigit():
+                d = int(char)
+                if d > f:
                     s_ = f
-                    f = int(char)
-                elif f > int(char)  and s_  < int(char):
-                    s_ = int(char) 
+                    f = d
+                elif f > d  > s_ :
+                    s_ = d 
                 
         return s_
