0c1ec35913479fa0055400ba6d853f9969fa8827
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-12-14 07:30:32 +0530

    Time: 0 ms (100%), Space: 18 MB (89.23%) - LeetHub

--- 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/1677-matrix-diagonal-sum.py
@@ -1,12 +1,17 @@
 class Solution:
     def diagonalSum(self, mat: List[List[int]]) -> int:
-        
-        res = 0
         n = len(mat)
+        return sum(
+            mat[i][i] + (mat[i][n-1-i] if i != n-1-i else 0)
+            for i in range(n)
+        )
 
-        for i in range(len(mat)):
-            res += mat[i][i]
-            if i != n-1-i:
-                res += mat[i][n-1-i]
+        # res = 0
+        # n = len(mat)
 
-        return res
+        # for i in range(len(mat)):
+        #     res += mat[i][i]
+        #     if i != n-1-i:
+        #         res += mat[i][n-1-i]
+
+        # return res
