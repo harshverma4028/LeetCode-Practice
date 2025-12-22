86bc7c5107090849512f5e8ee7f8123accaf25f6
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2025-12-22 22:31:36 +0530

    Time: 0 ms (100%), Space: 17 MB (100%) - LeetHub

---
+++ 1944-truncate-sentence/1944-truncate-sentence.py
@@ -0,0 +1,9 @@
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        s = s.split()
+
+        ns = []
+        for i in range(k):
+            ns.append(s[i])
+        
+        return " ".join(ns)
