7c13011c0fa9b3ddd8df9cdd182a3e89e3e45c91
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2026-01-17 19:46:42 +0530

    Time: 7 ms (19.79%), Space: 19.5 MB (7.35%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.py
@@ -1,15 +1,24 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        freq = {}
-
-        for i in range(len(nums)):
-            if nums[i] not in freq:
-                freq[nums[i]] = 1
+        s = set()
+        for num in nums:
+            if num not in s:
+                s.add(num)
             else:
-                freq[nums[i]] += 1
+                s.remove(num)
+
+        return len(s) == 0
+
+
+        # freq = {}
+        # for i in range(len(nums)):
+        #     if nums[i] not in freq:
+        #         freq[nums[i]] = 1
+        #     else:
+        #         freq[nums[i]] += 1
         
-        for val in freq.values():
-            if val % 2 == 1:
-                return False
+        # for val in freq.values():
+        #     if val % 2 == 1:
+        #         return False
             
-        return True
+        # return True
