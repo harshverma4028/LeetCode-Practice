72f03b1c12264117c550109d1b5739bab7b70021
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2026-01-24 19:24:38 +0530

    Time: 4 ms (5.03%), Space: 19.4 MB (17.59%) - LeetHub

--- 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.py
+++ 2377-check-if-number-has-equal-digit-count-and-digit-value/2377-check-if-number-has-equal-digit-count-and-digit-value.py
@@ -1,7 +1,19 @@
 class Solution:
     def digitCount(self, num: str) -> bool:
         
+        freq = {}
+
+        for ch in num:
+            freq[ch] = freq.get(ch, 0) + 1
+
         for i in range(len(num)):
-            if num.count(str(i)) != int(num[i]):
+            if freq.get(str(i), 0) != int(num[i]):
                 return False
+
         return True
+
+
+        # for i in range(len(num)):
+        #     if num.count(str(i)) != int(num[i]):
+        #         return False
+        # return True
