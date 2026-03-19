0410a923247cacb4059225c2dc0711db74b2eae9
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2026-03-19 21:32:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (51.65%) - LeetHub

--- 1714-rearrange-spaces-between-words/1714-rearrange-spaces-between-words.py
+++ 1714-rearrange-spaces-between-words/1714-rearrange-spaces-between-words.py
@@ -2,13 +2,17 @@ class Solution:
     def reorderSpaces(self, text: str) -> str:
         spaces = text.count(" ")
         words = text.split()
+        
 
         if len(words) == 1:
             return words[0] + " " * spaces
 
+
         gaps = len(words) - 1
+
 
         e_spaces = spaces // gaps
         ex_spaces = spaces % gaps
+
 
         return (" " * e_spaces).join(words) + " " * ex_spaces
