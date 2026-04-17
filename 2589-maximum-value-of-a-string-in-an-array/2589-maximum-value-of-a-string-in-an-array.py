c452c36d4af9fbdc7db4b819fa41b7888a5f4f62
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2026-04-17 20:36:46 +0530

    Time: 0 ms (100%), Space: 19.3 MB (48.56%) - LeetHub

--- 2589-maximum-value-of-a-string-in-an-array/2589-maximum-value-of-a-string-in-an-array.py
+++ 2589-maximum-value-of-a-string-in-an-array/2589-maximum-value-of-a-string-in-an-array.py
@@ -1,13 +1,16 @@
 class Solution:
     def maximumValue(self, strs: List[str]) -> int:
-        bada = 0
+        # bada = 0
 
-        for word in strs:
-            if word.isnumeric():
-                if int(word) > bada:
-                    bada = int(word)
-            else:
-                if len(word) > bada:
-                    bada = len(word)
+        # for word in strs:
+        #     if word.isnumeric():
+        #         if int(word) > bada:
+        #             bada = int(word)
+        #     else:
+        #         if len(word) > bada:
+        #             bada = len(word)
         
-        return bada
+        # return bada
+
+
+        return max(int(s) if s.isdigit() else len(s) for s in strs)
