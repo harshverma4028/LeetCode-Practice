90b13f888c6735d931290660ea105c16fd4d42bc
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2026-06-01 10:20:17 +0530

    Time: 0 ms (100%), Space: 19.3 MB (72.96%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,14 +1,12 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        res = ""
-        for i in range(len(strs[0])):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
-                    return res
-            res += strs[0][i] 
-        return res    
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
         
+        for i in range(len(strs[0])):
+            for j in range(1,len(strs)):
+
+                if i >= len(strs[j]) or strs[j][i] != strs[0][i]:
+                    return strs[0][:i]
+                
+        return strs[0]
