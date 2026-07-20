27321245ec2c61020661e673f1d51d6aad602aa4
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2026-07-20 23:45:22 +0530

    Time: 0 ms (100%), Space: 19.3 MB (64.68%) - LeetHub

---
+++ 1983-maximum-population-year/1983-maximum-population-year.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maximumPopulation(self, logs: List[List[int]]) -> int:
+        base = 1950
+        size = 101  # 1950..2050
+
+        diff = [0] * size
+
+        # Mark changes
+        for birth, death in logs:
+            diff[birth - base] += 1
+            diff[death - base] -= 1   # death is exclusive
+
+        # Prefix sum + track max
+        max_pop = 0
+        curr = 0
+        year = base
+
+        for i in range(size):
+            curr += diff[i]
+            if curr > max_pop:
+                max_pop = curr
+                year = base + i   # smallest year auto-handled
+
+        return year
