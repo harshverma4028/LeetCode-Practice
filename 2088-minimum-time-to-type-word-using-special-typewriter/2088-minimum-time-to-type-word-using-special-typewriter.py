5b1d789ac1c8b192fd4246b4f3d5ba1de128a04f
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2026-09-12 12:01:51 +0530

    Time: 0 ms (100%), Space: 19.3 MB (56.01%) - LeetHub

--- 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.py
+++ 2088-minimum-time-to-type-word-using-special-typewriter/2088-minimum-time-to-type-word-using-special-typewriter.py
@@ -3,11 +3,13 @@ class Solution:
         dis = 0
         current = 'a'
 
+
         for i in range(len(word)):
             target = word[i]
             normal_dis = abs(ord(current) - ord(target))
             cir_dis = 26 - normal_dis
             dis += min(cir_dis,normal_dis) + 1
             current = target
+
 
         return dis
