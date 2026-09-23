28bfeeb9382f5257f9ddecccc49953990d3dffc3
Harsh Kumar <97897760+harshverma4028@users.noreply.github.com>
2026-09-23 23:54:26 +0530

    Time: 0 ms (100%), Space: 19 MB (100%) - LeetHub

---
+++ 3893-generate-tag-for-video-caption/3893-generate-tag-for-video-caption.py
@@ -0,0 +1,16 @@
+class Solution:
+    def generateTag(self, caption: str) -> str:
+        words = caption.split()
+
+        res = "#"
+
+        for i in range(len(words)):
+            if i == 0:
+                res += words[i].lower()
+            else:
+                res += words[i].capitalize()
+
+            if len(res) == 100:
+                break
+
+        return res[:100]
